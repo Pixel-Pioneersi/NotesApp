9667a4d399c2af22576eab5efbf1f39829cfa5aa
Worteza <deniyache@gmail.com>
2023-11-09 19:30:49 +0200

    Adding the OnClick function and Data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-    implementation ("org.threeten:threetenbp:1.4.3")
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.20.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- app/src/main/java/com/example/notesapp/MainScreen.kt
+++ app/src/main/java/com/example/notesapp/MainScreen.kt
@@ -1,7 +1,7 @@
 package com.example.notesapp
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
@@ -27,22 +26,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import org.threeten.bp.Instant
-import org.threeten.bp.ZoneId
-import org.threeten.bp.format.DateTimeFormatter
 
 @Composable
 fun MainScreen() {
-    var itemsList by remember { mutableStateOf(emptyList<String>()) }
-    var expandedStates by remember { mutableStateOf(emptyList<Boolean>()) }
-
+    var itemsList by remember { mutableStateOf(List(0) { "Card $it" }) } // Initial list of items
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(R.drawable.background_img),
@@ -54,71 +46,34 @@ fun MainScreen() {
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
             contentPadding = PaddingValues(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             items(itemsList.size) { index ->
-                val cardCreationDate =
-                    Instant.now()
-
-                val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yy")
-                val formattedDate =
-                    cardCreationDate.atZone(ZoneId.systemDefault()).format(dateFormatter)
-
-                var cardSizes by remember { mutableStateOf(List(itemsList.size) { 1f }) }
-
                 ElevatedCard(
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .size(110.dp)
-                        .clickable {
-                            val currentIndex = index
-                            expandedStates = expandedStates.toMutableList().also {
-                                it[currentIndex] = !it[currentIndex]
-                            }
-                        }
-                ) {
-                    Box(modifier = Modifier.padding(8.dp)) {
-                        Text(
-                            text = itemsList[index],
-                            modifier = Modifier.align(Alignment.TopCenter),
-                            style = TextStyle(fontWeight = FontWeight.Bold)
-                        )
-
-                        Text(
-                            text = if (expandedStates[index]) itemsList[index] else itemsList[index].take(
-                                50
-                            ) + " ...",
-                            modifier = Modifier
-                                .padding(top = 15.dp, start = 0.dp)
-                                .scale(0.8f),
-                            style = TextStyle(fontWeight = FontWeight.Bold)
-                        )
-                    }
-                    Box(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxSize()
-                            .wrapContentSize(Alignment.BottomEnd)
-                            .scale(0.5f)
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    ),
+                    modifier = Modifier.padding(top = 16.dp).size(110.dp) ,
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.onSecondary,
+                    )
                     ) {
-                        Text(
-                            text = "$formattedDate",
-                            style = TextStyle(fontWeight = FontWeight.Normal)
-                        )
-                    }
+                    Text(
+                        text = itemsList[index],
+                        modifier = Modifier.padding(16.dp),
+                        style = TextStyle(fontWeight = FontWeight.Bold)
+                    )
                 }
             }
         }
 
         SmallFloatingActionButton(
             onClick = {
-                itemsList = itemsList + "New Card ${itemsList.size + 1}"
-                expandedStates = expandedStates + false
+                itemsList = itemsList + "New Card ${itemsList.size+1}"
             },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .safeDrawingPadding(),
+            modifier = Modifier.align(Alignment.BottomEnd).safeDrawingPadding(),
             containerColor = MaterialTheme.colorScheme.onPrimary
+
         ) {
             Icon(Icons.Filled.Add, "Floating action button.")
         }
